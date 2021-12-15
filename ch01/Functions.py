2e4953740ebb4a0b73de5d446f3a73b671a477c0
nureesong <nuri3136@naver.com>
2021-12-15 22:48:54 +0900

    upload the file

---
+++ ch01/Functions.py
@@ -0,0 +1,40 @@
+''' 
+06. OpenCV 주요 함수 설명
+'''
+
+import cv2
+
+# 1. 영상 파일 불러오기: np.ndarray로 리턴
+# img = cv2.imread(filename='./ch01/cat.bmp') # (default) flags = cv2.IMREAD_COLOR 
+img = cv2.imread(filename='./ch01/cat.bmp', flags=cv2.IMREAD_GRAYSCALE) # grayscale로 이미지 불러오기
+# img = cv2.imread(filename='./ch01/cat.bmp', flags=cv2.IMREAD_UNCHANGED) # png 파일의 알파 채널까지 불러온다.
+
+# 파일 확인
+import sys
+if img is None:
+    print('Image load failed!')
+    sys.exit()
+
+    
+# 2. 영상 파일 저장하기: 저장 여부(bool) 리턴
+cv2.imwrite(filename='./ch01/cat_gray.png', img=img)
+# params=[cv2.IMWRITE_JPEG_QUALITY, 90]: JPG 압축률을 90%로 지정
+
+    
+# 3. 새 창 띄우기: figure 창 고유 이름 설정
+cv2.namedWindow(winname='image') # 영상에 맞게 창 크기가 고정됨
+# flags=cv2.WINDOW_NORMAL: 창 크기 변경하면 그에 맞게 영상도 resize. 영상이 모니터 해상도보다 큰 경우 유용
+
+
+# 4. 영상 출력하기
+cv2.imshow(winname='image', mat=img)  # winname: 창 이름, mat: 띄울 이미지/영상
+
+
+# 아무 키를 누르면 화면에 띄워둔 이미지 창을 닫는다.
+cv2.waitKey()            # 키보드 입력을 기다리는 함수
+
+
+# 6. 창 닫기: 아래 코드 생략해도 엔터치면 창 닫힘
+cv2.destroyWindow(winname='image') # 지정한 창 하나만 닫는다.
+# cv2.destroyAllWindows()  # 열려있는 모든 창을 닫는다.
+
